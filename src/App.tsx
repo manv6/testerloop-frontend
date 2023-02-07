abed9615081821cd197cfe9e752b41e3f56e5e86
Bianca Rosca <bianca.rosca@overloop.io>
2023-02-07 15:47:46 +0200

    add panel for cypress error

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import { NetworkPanel } from './components/DevTools/Network';
 import { DomPreview } from './components/DomPreview';
 import { Steps } from './components/Steps';
 import * as Expandable from './components/Expandable';
+import { RelayEnvironment } from './gql/RelayEnvironment';
+import CypressSummary from './components/CypressSummary';
+
+import steps from './data/steps';
+import results from './data/results';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.scss';
-import { RelayEnvironment } from './gql/RelayEnvironment';
-import results from './data/results';
+
 
 function App() {
     // TODO: We likely want to add some "lead" and "lag" time to these dates,
     // so that events that occur at or near the very beginning or end of the
     // timeline can be viewed and scrubbed to easily.
 
-    const startTime = new Date(Date.parse(results.startedTestsAt));
+    const startTime = steps.at(0)!.options.wallClockStartedAt; // eslint-disable-line @typescript-eslint/no-non-null-assertion
     const endTime = new Date(Date.parse(results.endedTestsAt));
 
     return (
@@ -32,6 +37,9 @@ function App() {
                     <Expandable.Parent className={styles['expandable-parent']}>
                         <Expandable.Child className={styles['expandable-steps']} notExpandable={true}>
                             <Steps className={styles.steps} />
+                        </Expandable.Child>
+                        <Expandable.Child className={styles['expandable-cypress-summary']} notExpandable={true}>
+                            <CypressSummary />
                         </Expandable.Child>
                         <Expandable.Child className={styles['expandable-dom']}>
                             <DomPreview />
---
+++ src/components/CypressSummary/CypressSummary.tsx
@@ -0,0 +1,37 @@
+import cicd from 'src/data/cicd';
+import results from 'src/data/results';
+
+import styles from './CypressSummary.module.scss';
+
+const CypressSummary = () => {
+    const githubUrl = cicd.GITHUB_SERVER_URL;
+    const repository = cicd.GITHUB_REPOSITORY;
+    const ref = cicd.GITHUB_REF_NAME;
+
+    const errorObj = results.runs[0].tests[0].attempts[0];
+    const error = errorObj.error;
+
+    const relativePath = error.codeFrame.relativeFile;
+    const line = error.codeFrame.line;
+    const column = error.codeFrame.column;
+
+    const url = [githubUrl,repository, 'blob', ref, relativePath, `?#L${line}`].join('/');
+
+    const hrefText = [relativePath, line, column].join(':');
+
+    return <div className={styles.cypressSummary}>
+        {error ?
+            <div>
+                <div className={styles.title}>
+                    <span>!</span> {error.name}
+                </div>
+                <div className={styles.errorMessage}>{error.message}</div>
+                <a href={url} target="_blank" rel="noreferrer" >{hrefText}</a>
+            </div>
+            : <div>No errors</div>
+        }
+    </div>;
+
+};
+
+export default CypressSummary;
