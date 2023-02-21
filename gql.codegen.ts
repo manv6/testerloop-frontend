b6599f794ac32b4187358552c317e74547cbc97c
Bianca Rosca <bianca.rosca@overloop.io>
2023-02-21 16:23:17 +0200

    Revert urls to use prod link

--- gql.codegen.ts
+++ gql.codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: ['http://localhost:8080/api'],
+    schema: ['https://otf.overloop.io/api'],
     generates: {
         'src/gql/schema.gql': {
             plugins: ['schema-ast']
