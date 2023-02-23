d46439f8476560e1adc8599dc41a6bc41c719218
Bianca Rosca <bianca.rosca@overloop.io>
2023-02-23 13:15:36 +0200

    Fix console panel bugs

--- src/components/DevTools/ConsolePanel/ConsolePanel.tsx
+++ src/components/DevTools/ConsolePanel/ConsolePanel.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useTransition } from 'react';
 import { useRefetchableFragment } from 'react-relay';
 import { useTimeline } from 'src/hooks/timeline';
 import { LogEntry, LogFilters } from './components';
@@ -55,30 +55,43 @@ const ConsolePanel: React.FC<Props> = ({ fragmentKey }) => {
 
     const { currentTime, hoverTime } = useTimeline();
 
-    const [logSearch, setLogSearch] = React.useState<string>('');
+    const [logSearch, setLogSearch] = React.useState<string | null>(null);
     const debouncedResult = useDebounce(logSearch, 200);
     const debouncedTerm = debouncedResult[0];
 
-    useEffect(() => {
-        refetch({ logSearch: debouncedTerm });
-    }, [debouncedTerm, refetch]);
+    const [isPending, startTransition] = useTransition();
 
+    const defaultActiveLogLevels = fillObjFromType(LogLevel, true);
     const [activeLogLevels, setActiveLogLevels] = React.useState<
-        Record<LogLevel, boolean>
+        Record<LogLevel, boolean> | null
     >(
-        fillObjFromType(LogLevel, true)
+        null
     );
 
-    const toggleActiveLogLevel = (level: LogLevel) => {
+    useEffect(() => {
+        if(debouncedTerm === null && activeLogLevels === null){
+            return;
+        }
+        const logLevels = activeLogLevels
+            ? Object.keys(activeLogLevels).filter(
+                (key: string) => activeLogLevels[key as LogLevel])
+            : null;
+        startTransition(() => {
+            refetch({ logSearch: debouncedTerm, logLevels});
+        });
+    }, [activeLogLevels, debouncedTerm, refetch]);
+
+
+    const toggleActiveLogLevel = useCallback((level: LogLevel) => {
+        const activeLogs = activeLogLevels ?? defaultActiveLogLevels;
         const newActiveLogs = {
-            ...activeLogLevels,
-            [level]: !activeLogLevels[level],
+            ...activeLogs,
+            [level]: !activeLogs[level],
         };
-        setActiveLogLevels(newActiveLogs);
-        const logLevels = Object.keys(newActiveLogs).filter(
-            (key: string) => newActiveLogs[key as LogLevel]);
-        refetch({ logSearch: debouncedTerm, logLevels });
-    };
+        startTransition(() => {
+            setActiveLogLevels(newActiveLogs);
+        });
+    }, [activeLogLevels, defaultActiveLogLevels]);
 
     const logs = useMemo(
         () => data
@@ -116,25 +129,27 @@ const ConsolePanel: React.FC<Props> = ({ fragmentKey }) => {
         <section className={styles.consolePanel}>
             <LogFilters
                 logFilters={data}
-                filterTerm={logSearch}
+                filterTerm={logSearch ?? ''}
                 setFilterTerm={setLogSearch}
-                activeLogLevels={activeLogLevels}
+                activeLogLevels={activeLogLevels ?? defaultActiveLogLevels}
                 toggleActiveLogLevel={toggleActiveLogLevel}
             />
 
-            <ul className={styles.logsList}>
-                {(logs || [])?.map((node, idx) => {
+            {isPending
+                ? <div>Loading...</div>
+                : (<ul className={styles.logsList}>
+                    {(logs || [])?.map((node, idx) => {
                     // TODO: Timestamp is not unique, provide an id or a way to make it unique.
-                    return (
-                        <LogEntry
-                            key={idx}
-                            isLogSelected={currentLogIdx === idx}
-                            isLogHovered={hoveredLogIdx === idx}
-                            logEntry={node}
-                        />
-                    );
-                })}
-            </ul>
+                        return (
+                            <LogEntry
+                                key={idx}
+                                isLogSelected={currentLogIdx === idx}
+                                isLogHovered={hoveredLogIdx === idx}
+                                logEntry={node}
+                            />
+                        );
+                    })}
+                </ul>)}
         </section>
     );
 };
