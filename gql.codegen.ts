b7a8094e62358b2d1804958e63a2dae9d7246e42
Bianca Rosca <bianca.rosca@overloop.io>
2023-04-20 15:03:52 +0300

    Fix link mistake

--- gql.codegen.ts
+++ gql.codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: ['http://otf.overloop.io/api'],
+    schema: ['https://otf.overloop.io/api'],
     generates: {
         'src/gql/schema.gql': {
             plugins: ['schema-ast'],
