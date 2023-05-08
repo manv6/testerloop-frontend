e135984d8a240f859163642d96938b877e78df79
Oliver Wright <oliver@overloop.io>
2023-05-08 12:17:40 +0100

    Add default Query.node impl (#82)

--- src/gql/environment.ts
+++ src/gql/environment.ts
@@ -1,4 +1,13 @@
-import type { FetchFunction } from 'relay-runtime';
+import {
+    DataID,
+    FetchFunction,
+    NormalizationLinkedField,
+    ROOT_ID,
+    ROOT_TYPE,
+    ReadOnlyRecordProxy,
+    ReaderLinkedField,
+    Variables,
+} from 'relay-runtime';
 import {
     Store,
     RecordSource,
@@ -6,6 +15,7 @@ import {
     Network,
     Observable,
 } from 'relay-runtime';
+import { ReadOnlyRecordSourceProxy } from 'relay-runtime/lib/store/RelayStoreTypes';
 
 const ftch: FetchFunction = (params, variables) => {
     const response = fetch('/api', {
@@ -35,5 +45,33 @@ const ftch: FetchFunction = (params, variables) => {
 export function createEnvironment() {
     const network = Network.create(ftch);
     const store = new Store(new RecordSource());
-    return new Environment({ store, network });
+    return new Environment({
+        store,
+        network,
+        missingFieldHandlers: [
+            {
+                kind: 'linked',
+                handle(
+                    field: NormalizationLinkedField | ReaderLinkedField,
+                    parentRecord: ReadOnlyRecordProxy | null | undefined,
+                    args: Variables,
+                    store: ReadOnlyRecordSourceProxy
+                ): DataID | null | undefined {
+                    if (!parentRecord) return;
+                    // NOTE: TS defs for parentRecord are incorrect.
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    const parent = parentRecord as any;
+                    if (
+                        parent.__typename !== ROOT_TYPE ||
+                        parent.__id !== ROOT_ID
+                    )
+                        return;
+                    if (field.name !== 'node') return;
+                    if (typeof args.id !== 'string') return;
+                    const { id } = args;
+                    return id;
+                },
+            },
+        ],
+    });
 }
