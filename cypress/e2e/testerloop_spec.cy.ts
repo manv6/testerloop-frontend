f4a835b0a317ec22ecbd409e4fc95e7b0a4fcc08
Kalli Kavasi <kalli.kavasi@carbon.ci>
2023-07-03 16:02:47 +0300

    test for run page

--- cypress/e2e/testerloop_spec.cy.ts
+++ cypress/e2e/testerloop_spec.cy.ts
@@ -10,8 +10,16 @@ describe('testerloop debug page', () => {
     const btnCypressError = '[data-cy="filter-cypressError"]';
     const btnNetworkError = '[data-cy="filter-networkError"]';
     const btnNetworkSuccess = '[data-cy="filter-networkSuccess"]';
+    const runId = '[data-cy="run-summary-title"]';
+    const testCount = '[data-cy="test-count"]';
+    const browser = '[data-cy="test-run-browser"]';
+    const branch = '[data-cy="test-run-branch"]';
+    const runDatetime = '[data-cy="test-run-datetime"]';
+    const author = '[data-cy="test-run-author"]';
+    const testExecutionNo = (no: number) => `[data-cy="test-execution-${no}"]`;
+    const testsTable = '[data-cy="table"]';
 
-    it('verify Testerloop Contact page', () => {
+    it('verify Testerloop Debug Contact page', () => {
         cy.fixture('contactPage').then((data) => {
             cy.visit(data.url);
             //verify summary
@@ -35,7 +43,6 @@ describe('testerloop debug page', () => {
             cy.get(btnNetworkSuccess).should('have.text', 'Success network');
 
             //verify step definitions
-            cy.get('#stepsHeader').should('have.text', 'Scenario');
             cy.contains("I visit overloop's website").should('be.visible');
             cy.contains('I navigate to contact page').should('be.visible');
             cy.contains('I fill in contact form details').should('be.visible');
@@ -50,6 +57,45 @@ describe('testerloop debug page', () => {
             cy.contains(
                 'SENTRY WAS __NOT__ INITIALIZED. Logs will be forwarded to console instead.'
             ).should('be.visible');
+        });
+    });
+
+    it('verify Testerloop Run page', () => {
+        cy.fixture('runPage').then((data) => {
+            cy.visit(data.url);
+            //verify summary
+            cy.get(runId).should('have.text', data.runId);
+            cy.get(status).first().should('have.text', data.status);
+            cy.get(testCount).should('have.text', data.noTests);
+            cy.get(branch).should('have.text', data.branch);
+            cy.get(runDatetime).should('have.text', data.dateTime);
+            cy.get(author).should('have.text', data.author);
+            //verify table
+            cy.get('tr').should('have.length', 4);
+            cy.get(testExecutionNo(0))
+                .find('td')
+                .eq(0)
+                .contains(data.textExecution0Name)
+                .should('be.visible');
+            cy.get(testExecutionNo(0))
+                .find('td')
+                .eq(1)
+                .contains(data.textExecution0Datetime)
+                .should('be.visible');
+            cy.get(testExecutionNo(0))
+                .find('td')
+                .eq(2)
+                .contains(data.textExecution0Duration)
+                .should('be.visible');
+            cy.get(testExecutionNo(0))
+                .find('td')
+                .eq(3)
+                .contains(data.textExecution0Status)
+                .should('be.visible');
+            cy.get(testExecutionNo(0)).click();
+            cy.fixture('contactPage').then((contact) => {
+                cy.url().should('eq', contact.url);
+            });
         });
     });
 });
