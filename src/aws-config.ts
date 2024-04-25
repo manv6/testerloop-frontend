1a2eb11880531033919e6d127ba55ca0c930e6b0
manv6 <evlastos@gmail.com>
2024-04-25 18:36:37 +0300

    Getting creds from .env file

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -1,19 +1,20 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import AWS from 'aws-sdk';
+const env = process.env;
 
 if (
-    !process.env.REACT_APP_AWS_ACCESS_KEY_ID ||
-    !process.env.REACT_APP_AWS_SECRET_ACCESS_KEY ||
-    !process.env.REACT_APP_AWS_REGION
+    !env.REACT_APP_AWS_ACCESS_KEY_ID ||
+    !env.REACT_APP_AWS_SECRET_ACCESS_KEY ||
+    !env.REACT_APP_AWS_REGION
 ) {
     throw new Error('AWS configuration environment variables must be set');
 }
 
 const config = {
-    region: process.env.REACT_APP_AWS_REGION!,
+    region: env.REACT_APP_AWS_REGION!,
     credentials: {
-        accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY!,
+        accessKeyId: env.REACT_APP_AWS_ACCESS_KEY_ID!,
+        secretAccessKey: env.REACT_APP_AWS_SECRET_ACCESS_KEY!,
     },
 };
 
